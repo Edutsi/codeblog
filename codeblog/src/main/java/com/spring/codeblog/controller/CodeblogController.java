52614f0931784fd62f190a4d172d51bb57aea624
edutsi <nilson.upis@gmail.com>
2021-03-02 07:37:51 -0300

    AWS 09: Salvando um novo Post

--- codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -1,13 +1,18 @@
 package com.spring.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
@@ -33,6 +38,23 @@ public class CodeblogController {
 		mv.addObject("post", post);//o div class card do posts.hmtl busca este obj th:each 
 		return mv;
 	}
+	///metodo para criar um novo post e redirecionar para o formulário postform
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	//@valid vai fazer as validacoes do post como a @Blank
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()){
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		codeblogService.save(post);
+		return "redirect:/posts";
+	}
+	
 }
 
 
